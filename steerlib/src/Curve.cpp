c288f71e955a227da9ef28896803d2c19184f92c
ZaneQi <qizheng1993hit@gmail.com>
2016-09-17 15:15:39 -0400

    CheckRobust

--- steerlib/src/Curve.cpp
+++ steerlib/src/Curve.cpp
@@ -111,16 +111,15 @@ bool Curve::calculatePoint(Point& outputPoint, float time)
 bool Curve::checkRobust()
 {
 	//================DELETE THIS PART AND THEN START CODING===================
-	static bool flag = false;
+	/*static bool flag = false;
 	if (!flag)
 	{
 		std::cerr << "ERROR>>>>Member function checkRobust is not implemented!" << std::endl;
 		flag = true;
-	}
+	}*/
 	//=========================================================================
-
-
-	return true;
+	return controlPoints.size()>=2;
+	//return true;
 }
 
 // Find the current time interval (i.e. index of the next control point to follow according to current time)
